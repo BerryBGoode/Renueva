e1ee834a412c3f5fad09f74227d20c22bce10110
BerryBGoode <fernandomena3131@gmail.com>
2023-05-17 21:35:48 -0600

    contador en articulo

--- controllers/public/article.js
+++ controllers/public/article.js
@@ -6,6 +6,9 @@ const CART = 'business/public/cart.php';
 const PATH = '../../api/images/products/';
 // componente donde renderizar comentarios
 const COMMENTS = document.getElementById('container-comments');
+
+// var para contador
+let count = document.getElementById('count');
 /**
  * Método para obtener el producto que se consulta, este producto viene de la url
  */
@@ -40,6 +43,11 @@ document.addEventListener('DOMContentLoaded', async event => {
 
         // comentarios
         loadComments(event);
+
+        // contador
+        count.innerText = 1;
+        count.value = 1;
+        quantity();
 
     } else {
         M.toast({ html: 'Error to get this product!!' });
@@ -76,4 +84,24 @@ const loadComments = async event => {
                 `;
             });
         }
+}
+
+const quantity = () =>{
+
+    // obtener boton para quitar cantidad y crear evento click
+    document.getElementById('rest').addEventListener('click', () => {
+        // verificar sí el contandor es mayor o igual 1 
+        if (count.value > 1) {
+            // poder restar cantidad
+            count.value = count.value - 1;
+            count.innerText = count.value;
+        }
+        
+    })
+
+    // obtener boton para agregar cantidad y crear evento click
+    document.getElementById('sum').addEventListener('click', () => {
+        count.value = count.value + 1;
+        count.innerText = count.value;
+    })
 }
