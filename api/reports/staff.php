c669a9759343301d6833ebbae9f85a459617bf3d
Rober008 <20210722@ricaldone.edu.sv>
2023-07-19 23:36:20 -0600

    avance de reporte de staff

---
+++ api/reports/staff.php
@@ -0,0 +1,10 @@
+<?php
+//
+require_once('../../api/helpers/reports.php');
+//
+require_once('../../api/entities/access/staff.php');
+require_once('../../api/entities/transfers/staff.php');
+
+//
+$pdf = new Report;
+//
