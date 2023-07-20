c9d3bdc766dfe133556574a168fa8818b6daa770
Rober008 <20210722@ricaldone.edu.sv>
2023-07-19 23:48:54 -0600

    staff report

--- api/reports/clients.php
+++ api/reports/clients.php
@@ -1,14 +1,14 @@
 <?php 
-//
+// Se agrega la clase con las plantillas que generan los reportes
 require_once('../../api/helpers/reports.php');
-//
+// Se agregan las clases para el acceso y transferencias de datos.
 require_once('../../api/entities/transfers/client.php');
 require_once('../../api/entities/access/client.php');
 
 //
 $pdf = new Report;
 //
-$pdf->reportHeader('Usuarios de clientes.');
+$pdf->reportHeader('Customer users.');
 //
 $client = new Client;
 $clients = new ClientQuery;
@@ -21,7 +21,7 @@ if ($dataClients = $clients->all()) {
     //
     $pdf->cell(62, 10, 'Username', 1, 0, 'C', 1);
     $pdf->cell(62,  10, 'Name client', 1, 0, 'C', 1);
-    $pdf->cell(62, 10, 'Last name Client', 1, 0, 'C', 1);
+    $pdf->cell(62, 10, 'phone', 1, 0, 'C', 1);
 
     //
     $pdf->SetFillColor(225);
@@ -31,11 +31,11 @@ if ($dataClients = $clients->all()) {
     //
     foreach ($dataClients as $rowClients) {
         $pdf->cell(62, 10, $pdf->stringEncoder($rowClients['username']), 1, 0);
-        $pdf->cell(62, 10, $pdf->stringEncoder($rowClients['names']), 1, 0);
-        $pdf->cell(62, 10, $pdf->stringEncoder($rowClients['last_names']), 1, 0);
+        $pdf->cell(62, 10, $pdf->stringEncoder($rowClients['names'] . ' ' . $rowClients['last_names']), 1, 0);
+        $pdf->cell(62, 10, $rowClients['phone'], 1, 0);
     }
 }else{
-    $pdf->cell(0, 10, $pdf->stringEncoder('No hay clientes para mostrar'));
+    $pdf->cell(0, 10, $pdf->stringEncoder('No clients to show.'));
 }
 //
 $pdf->Output('I', 'clients.pdf');
--- api/reports/staff.php
+++ api/reports/staff.php
@@ -8,3 +8,35 @@ require_once('../../api/entities/transfers/staff.php');
 //
 $pdf = new Report;
 //
+$pdf->reportHeader('Staff users.');
+//
+$staff = new Staff;
+$staffs = new StaffQuery;
+//
+if ($dataStaff = $staffs->all()) {
+    //
+    $pdf->SetFillColor(175);
+    //
+    $pdf->SetFont('Times', 'B', 11);
+    //
+    $pdf->cell(62, 10, 'Username', 1, 0, 'C', 1);
+    $pdf->cell(62,  10, 'Name client', 1, 0, 'C', 1);
+    $pdf->cell(62, 10, 'phone', 1, 0, 'C', 1);
+
+    //
+    $pdf->SetFillColor(225);
+    //
+    $pdf->SetFont('Times', 'B', 11);
+
+    //
+    foreach ($dataStaff as $rowStaff) {
+        //
+        $pdf->cell(62, 10, $pdf->stringEncoder($rowClients['username']), 1, 0);
+        $pdf->cell(62, 10, $pdf->stringEncoder($rowClients['names'] . ' ' . $rowClients['last_names']), 1, 0);
+        $pdf->cell(62, 10, $rowClients['phone'], 1, 0);
+    }
+} else {
+    $pdf->cell(0, 10, $pdf->stringEncoder('No staff to show.'));
+}
+//
+$pdf->Output('I', 'staff.pdf');
